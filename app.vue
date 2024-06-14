50b03f1fe2740e37b9568e16f791c5aa4678a29f
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-06-14 21:47:27 +0330

    Fix: Handle page and layout in app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <main>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+  </main>
 </template>
